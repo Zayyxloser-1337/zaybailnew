0d7ebf28ab98d392fee1b1d9634128294e011419
kiuur <kyuurzy@gmail.com>
2025-11-09 08:53:10 +0800

    ESM

---
+++ WAProto/lib/index.d.ts
@@ -0,0 +1,22 @@
+
+/**
+  * Copyright © 2025 [ KyuuRzy ]
+  * All rights reserved. This source code is the property of [ ANU Team ].
+  *
+  * GitHub: https://github.com/kiuur
+  * Telegram: https://t.me/tskiofc
+  * Note: jangan hpus nama gw _-
+*/
+
+import makeWASocket from './Socket/index.js';
+export * from '../WAProto/index.js';
+export * from './Utils/index.js';
+export * from './Types/index.js';
+export * from './Defaults/index.js';
+export * from './WABinary/index.js';
+export * from './WAM/index.js';
+export * from './WAUSync/index.js';
+export type WASocket = ReturnType<typeof makeWASocket>;
+export { makeWASocket };
+export default makeWASocket;
+//# sourceMappingURL=index.d.ts.map
---
+++ WAProto/lib/index.js
@@ -0,0 +1,27 @@
+
+/**
+  * Copyright © 2025 [ KyuuRzy ]
+  * All rights reserved. This source code is the property of [ ANU Team ].
+  *
+  * GitHub: https://github.com/kiuur
+  * Telegram: https://t.me/tskiofc
+  * Note: don't delete my nick lol 
+*/
+
+import makeWASocket from './Socket/index.js';
+export * from '../WAProto/index.js';
+export * from './Utils/index.js';
+export * from './Types/index.js';
+export * from './Defaults/index.js';
+export * from './WABinary/index.js';
+export * from './WAM/index.js';
+export * from './WAUSync/index.js';
+export { makeWASocket };
+export default makeWASocket;
+
+import chalk from "chalk"
+console.log(chalk.magentaBright.bold("\n✨ Modified Baileys ✨\n"));
+console.log(chalk.whiteBright("Hi, thank you for using my modified Baileys ^-^"));
+console.log(chalk.cyan("Telegram: ") + chalk.greenBright("@tskiofc"));
+console.log(chalk.gray("------------------------------\n"));
+//# sourceMappingURL=index.js.map
